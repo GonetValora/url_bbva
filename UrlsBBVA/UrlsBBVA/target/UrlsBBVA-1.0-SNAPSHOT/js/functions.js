248344c278091ff799e156d6bcdc9ae22216682b
mb67011 <isaacmanuel.pulido.garcia@bbva.com>
2016-08-11 17:08:09 -0500

    Terminado

--- UrlsBBVA/UrlsBBVA/target/UrlsBBVA-1.0-SNAPSHOT/js/functions.js
+++ UrlsBBVA/UrlsBBVA/target/UrlsBBVA-1.0-SNAPSHOT/js/functions.js
@@ -223,6 +223,8 @@ function ValidarOtp(dato) {
             } else {
                 return false;
             }
+        } else {
+            return false;
         }
     } catch (e) {
         return false;
